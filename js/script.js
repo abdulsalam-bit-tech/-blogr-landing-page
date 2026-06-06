bdd1f03684f458d3dd7f6f346e26229653af8f4b
abdulsalam-bit-tech <abdulsalamsheriffdin@gmail.com>
2026-06-06 12:44:36 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("Blogr Landing Page Loaded Successfully");
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
